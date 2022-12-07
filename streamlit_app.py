91cb1dd2caddc41031047df2451fd7a2ce39c758
XiaoboAShi <119514584+XiaoboAShi@users.noreply.github.com>
2022-12-07 08:13:37 -0500

    Update streamlit_app.py
    change to use + "kiwi"

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # .json writes the content on screen
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) # .json writes the content on screen
 
 #Normalize semi-structured json data to a flat table:
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
