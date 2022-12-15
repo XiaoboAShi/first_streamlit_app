aed7d7eadda7b16f0216b243247414fb4cb08f13
XiaoboAShi <119514584+XiaoboAShi@users.noreply.github.com>
2022-12-15 16:37:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,22 +30,21 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-'''
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #Text entry box to get user input
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') #Kiwi is a default value to avoid err
-streamlit.write('The user entered = ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi') #Kiwi is a default value to avoid err
+#streamlit.write('The user entered = ', fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #streamlit.text(fruityvice_response.json()) # .json writes the content on screen
 #Normalize semi-structured json data to a flat table:
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 #Display json content in a table with col names:
-streamlit.dataframe(fruityvice_normalized)
-'''
+#streamlit.dataframe(fruityvice_normalized)
+
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?') 
   if not fruit_choice:
